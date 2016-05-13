655b22a49bc1275a609dd0bff07e40ef2f2f7985
feupeu <nkmouritzsen@gmail.com>
2016-05-13 15:37:26 +0200

    Add minor changes to code snippers

--- content/security/code_snippets/notifyQuestionnaireCompleted.java
+++ content/security/code_snippets/notifyQuestionnaireCompleted.java
@@ -1,20 +1,26 @@
-public void notifyQuestionnaireCompleted(final long snapshotTimeStamp, Questionnaire questionnaire) {
-  Realm realm = null;
-  try {
-    realm = Realm.getDefaultInstance();//*\label{lst:get_default_realm}
-    final Snapshot snapshot = realm.where(Snapshot.class).equalTo("timestamp", snapshotTimeStamp).findFirst(); 
-    if (snapshot != null) {
-      realm.beginTransaction();
-      questionnaire = realm.copyToRealm(questionnaire);
-      realm.commitTransaction();
-      realm.beginTransaction();
-      snapshot.setQuestionnaire(questionnaire);
-      realm.copyToRealmOrUpdate(snapshot);
-      realm.commitTransaction();
-    }
-  } finally {
-    if (realm != null) {
-      realm.close();
-    }
-  }
+public void notifyQuestionnaireCompleted(final long snapshotTimeStamp, final Questionnaire questionnaire) {
+	Realm realm = null;
+	try {
+		realm = Realm.getDefaultInstance();//*\label{lst:get_default_realm}
+
+		final Snapshot snapshot = realm.where(Snapshot.class).equalTo("timestamp", snapshotTimeStamp).findFirst(); 
+
+		// If we found a snapshot
+		if (snapshot != null) {
+			// Save the questionnaire
+			realm.beginTransaction();
+			questionnaire = realm.copyToRealm(questionnaire);
+			realm.commitTransaction();
+
+			// Associate the questionnaire to the snapshots, and update it 
+			realm.beginTransaction();
+			snapshot.setQuestionnaire(questionnaire);
+			realm.copyToRealmOrUpdate(snapshot);
+			realm.commitTransaction();
+		}
+	} finally {
+		if (realm != null) {
+			realm.close();
+		}
+	}
 }
