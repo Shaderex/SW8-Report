a22499598d6f060d80b533198333d9299a5cc6b1
feupeu <nkmouritzsen@gmail.com>
2016-05-17 13:32:21 +0200

    Write section "Gathering Data from the Microsoft Band 2"

---
+++ content/gathering_sensor_data/code_snippets/get_connected_band_client.java
@@ -0,0 +1,20 @@
+protected boolean getConnectedBandClient() throws InterruptedException, BandException {
+  if (bandClient == null) {
+    final BandInfo[] devices = BandClientManager.getInstance().getPairedBands();
+    if (devices.length == 0) {
+      Log.d("Band2", "Band isn't paired with your phone (from " + this.getClass().getName() + ").");
+      return false;
+    }
+    final Context context = contextWeakReference.get();
+    bandClient = BandClientManager.getInstance().create(context, devices[0]);
+  } else if (ConnectionState.CONNECTED == bandClient.getConnectionState()) {
+    return true;
+  }
+
+  Log.d("Band2", "Band is connecting...  (from " + this.getClass().getName() + ")");
+  final ConnectionState result = bandClient.connect().await();
+
+  Log.d("Band2", "Band connection status: " + result + " (from " + this.getClass().getName() + ")");
+
+  return ConnectionState.CONNECTED == result;
+}
