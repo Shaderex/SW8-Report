aab35ed252381da6735425159e4db41d2ae4b986
marhlder <marhlder@gmail.com>
2016-05-25 11:23:33 +0200

    fix ref to band connect code and add more description

--- content/gathering_sensor_data/code_snippets/get_connected_band_client.java
+++ content/gathering_sensor_data/code_snippets/get_connected_band_client.java
@@ -2,11 +2,11 @@ protected boolean getConnectedBandClient() throws InterruptedException, BandExce
   if (bandClient == null) {
     final BandInfo[] devices = BandClientManager.getInstance().getPairedBands();
     if (devices.length == 0) {
-      Log.d("Band2", "Band isn't paired with your phone (from " + this.getClass().getName() + ").");
+      Log.d("Band2", "Band isn't paired with your phone (from " + this.getClass().getName() + ")."); //*\label{line:debug_log}
       return false;
     }
     final Context context = contextWeakReference.get();
-    bandClient = BandClientManager.getInstance().create(context, devices[0]);
+    bandClient = BandClientManager.getInstance().create(context, devices[0]); //*\label{line:device_array_zero_index}
   } else if (ConnectionState.CONNECTED == bandClient.getConnectionState()) {
     return true;
   }
